d8f13598a65224eae24f41a86e91a983447abd2f
Rilban <114222307+Rilban@users.noreply.github.com>
2023-10-23 12:59:36 +0200

    Separate encode and decode tests

--- tests/encoders_test.py
+++ tests/encoders_test.py
@@ -1,1 +1,12 @@
 import unittest
+import pytest
+from py_enc.utils import encode_data_to_pixel
+
+class PixelDataEncodeTests(unittest.TestCase):
+    def test_can_encode_pixel(self):
+
+        result = encode_data_to_pixel((38,120,255,0), 0b01011101)
+
+        expected = (37, 121, 255, 1)
+
+        self.assertEqual(result, expected)
